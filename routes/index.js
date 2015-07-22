370222705118586c594d9dc90385cf86999150e2
Anna Casper <anna.casper3@gmail.com>
2015-07-22 14:18:51 -0600

    index route create

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//GET concerts index
+router.get('/concerts', function(req, res, next){
+  res.render('concerts');
+});
+
 module.exports = router;
